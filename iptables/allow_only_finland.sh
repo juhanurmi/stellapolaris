19d38bd910ad5985a250ccee43ef68ed99a48b95
juhanurmi <juha.nurmi@ahmia.fi>
2017-02-23 09:42:00 +0200

    IPTABLES.

--- iptables/allow_only_finland.sh
+++ iptables/allow_only_finland.sh
@@ -8,11 +8,17 @@ wget http://www.ipdeny.com/ipblocks/data/countries/fi.zone
 # Allow these ip blocks
 while read ipblock;
 do
-    iptables -A INPUT -p tcp --dport 44567 -s $ipblock -j ACCEPT
+  iptables -A INPUT -s $ipblock -j ACCEPT
+  #Example, 44567/TCP
+  #iptables -A INPUT -p tcp --dport 44567 -s $ipblock -j ACCEPT
 done < fi.zone
 
 # Allow localhost
-iptables -A INPUT -p tcp --dport 44567 -s localhost -j ACCEPT
+iptables -A INPUT -s localhost -j ACCEPT
+#Example, 44567/TCP
+#iptables -A INPUT -p tcp --dport 44567 -s localhost -j ACCEPT
 
 # Drop everything else
-iptables -A INPUT -p tcp --dport 44567 -j DROP
+iptables -A INPUT -j DROP
+#Example, 44567/TCP
+#iptables -A INPUT -p tcp --dport 44567 -j DROP
