79708215e3fa1e18ed14d99705389f80c72d40c0
juhanurmi <juha.nurmi@ahmia.fi>
2017-02-23 09:59:10 +0200

    IPTABLES.

--- iptables/allow_only_finland.sh
+++ iptables/allow_only_finland.sh
@@ -5,20 +5,23 @@
 # Download a fresh zone file
 wget http://www.ipdeny.com/ipblocks/data/countries/fi.zone
 
+ORPort=443
+DirPort=9001
+
 # Allow these ip blocks
 while read ipblock;
 do
-  iptables -A INPUT -s $ipblock -j ACCEPT
-  #Example, 44567/TCP
-  #iptables -A INPUT -p tcp --dport 44567 -s $ipblock -j ACCEPT
+  #iptables -A INPUT -s $ipblock -j ACCEPT
+  iptables -A INPUT -p tcp --dport $DirPort -s $ipblock -j ACCEPT
+  iptables -A INPUT -p tcp --dport $ORPort -s $ipblock -j ACCEPT
 done < fi.zone
 
 # Allow localhost
-iptables -A INPUT -s localhost -j ACCEPT
-#Example, 44567/TCP
-#iptables -A INPUT -p tcp --dport 44567 -s localhost -j ACCEPT
+#iptables -A INPUT -s localhost -j ACCEPT
+iptables -A INPUT -p tcp --dport $DirPort -s localhost -j ACCEPT
+iptables -A INPUT -p tcp --dport $ORPort -s localhost -j ACCEPT
 
 # Drop everything else
-iptables -A INPUT -j DROP
-#Example, 44567/TCP
-#iptables -A INPUT -p tcp --dport 44567 -j DROP
+#iptables -A INPUT -j DROP
+iptables -A INPUT -p tcp --dport $DirPort -j DROP
+iptables -A INPUT -p tcp --dport $ORPort -j DROP
